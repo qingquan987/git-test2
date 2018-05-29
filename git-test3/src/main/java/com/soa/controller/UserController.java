bc7bf79eb9dcc2c08dfeadf4097699290f3e3681
chend nick <263251219@qq.com>
2018-05-29 10:11:06 +0800

    添加项目内容

---
+++ git-test3/src/main/java/com/soa/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.soa.controller;
+
+/**
+ * Created by nick on 2018/5/29.
+ */
+public class UserController {
+    public void findUserById(){
+        System.out.println("findUserById");
+    }
+}
